825ae55b1e554cdedf7f447291bffe9a85c2aa9e
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2018-11-16 04:36:31 +0900

    Add a minimal Babel + TypeScript + Webpack + React example :)

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    index: './src/index.tsx'
+  },
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Babel + TypeScript + React = ❤️',
+      template: 'src/index.html'
+    })
+  ]
+}
