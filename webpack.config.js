f918737d8cd35d177608e6bfa65418999a13006a
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-04-14 15:35:24 +0900

    Refactoring: simplify npm-scripts (npm start)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.tsx',
   },
