9ba46c690cc3d579e6cb10cb7ad31636166acb77
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-30 06:01:40 +0900

    Refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-render(
-  <App />,
-  document.getElementById('app') as HTMLElement,
-);
+ReactDOM.render(<App />, document.getElementById('app'));
